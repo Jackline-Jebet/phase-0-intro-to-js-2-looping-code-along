4818ccb956a852fd0cf429035dd67ffec1428746
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-10 07:23:02 +0300

    commit tree 31eef03c97823b518a75e821ef75d487307d33e3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards( namesArray, event ) {
-    let thankYouCards = []
+    const thankYouCards = []
     for ( let i = 0; i < namesArray.length; i++ ) {
       thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
     }
